879a76f7745ff9347c11932ce34f9d17552bc24a
KaikeDev <102434357+KaikeDev@users.noreply.github.com>
2024-09-29 17:00:52 -0300

    Adicionando projeto JWT

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet, RouterLink],
+  template: `
+
+
+  <router-outlet />
+   `,
 })
-export class AppComponent {
-  title = 'auth-front';
-}
+export class AppComponent {}
